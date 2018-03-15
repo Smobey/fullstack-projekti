66d224d71b800f98dd68e13b415351986b2cb181
Topi T <smobey@gmail.com>
2018-03-15 14:45:48 +0200

    playing with redux!

--- fullstack-rogue-front/src/index.js
+++ fullstack-rogue-front/src/index.js
@@ -1,10 +1,45 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {createStore} from 'redux'
 
-const App = () => (
-  <div>
-    <p>Hello world</p>
-  </div>
-)
+const counterReducer = (state = 0, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return state + 1
+        case 'DECREMENT':
+            return state - 1
+        case 'ZERO':
+            return 0
+    }
+    return state
+}
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const store = createStore(counterReducer)
+
+class App extends React.Component {
+    render() {
+      return(
+        <div>
+          <div>
+            {store.getState()}
+          </div>
+          <button onClick={e => store.dispatch({ type: 'INCREMENT'})}>
+            plus
+          </button>
+          <button onClick={e => store.dispatch({ type: 'DECREMENT' })}>
+            minus
+          </button>
+          <button onClick={e => store.dispatch({ type: 'ZERO' })}>
+            zero
+          </button>
+        </div>
+      )
+    }
+  }
+  
+  const renderApp = () => {
+    ReactDOM.render(<App />, document.getElementById('root'))
+  }
+  
+  renderApp()
+  store.subscribe(renderApp)
